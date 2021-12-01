f10b0c06b39e85324364b1568681ab72d299427b
Dmitrii Okunev <xaionaro@dx.center>
2021-12-01 21:07:10 +0000

    Add push-to-dockerhub.sh

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ EDK2_VERSION=latest
 all:
 	DOCKER_TAG=$(EDK2_VERSION) DOCKERFILE_PATH=Dockerfile IMAGE_NAME=xaionaro2/edk2-builder:$(EDK2_VERSION) hooks/build
 
+push-to-dockerhub:
+	./push-to-dockerhub.sh
+
 clean:
 	docker rmi xaionaro2/edk2-builder
---
+++ push-to-dockerhub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+push() {
+	TAG="$1"; shift
+	IMAGE_NAME=xaionaro2/edk2-builder:"$TAG"
+
+	docker rmi "$IMAGE_NAME" 2>/dev/null || true
+	EDK2_VERSION="$TAG" DOCKERFILE_PATH=Dockerfile IMAGE_NAME="$IMAGE_NAME" hooks/build
+	docker push "$IMAGE_NAME"
+	docker rmi "$IMAGE_NAME"
+}
+
+push "latest"
+
+for TAG in $(git ls-remote --tags https://github.com/tianocore/edk2 | awk '{print $2}' | sed -e 's%refs/tags/%%g' | grep -v '{}'); do
+	IMAGE_NAME=xaionaro2/edk2-builder:"$TAG"
+	if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect "$IMAGE_NAME"; then
+		continue
+	fi
+	push "$TAG"
+done
+
