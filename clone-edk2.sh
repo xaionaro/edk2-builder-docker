8568722b5f2dd664dd3f94a67abe4a056d1a74d9
startergo <7897244+startergo@users.noreply.github.com>
2021-12-02 16:34:21 -0500

    Update clone-edk2.sh
    Added AcidantheraAUDK

--- clone-edk2.sh
+++ clone-edk2.sh
@@ -9,9 +9,12 @@ case "$EDK2_VERSION" in
 		git clone "https://github.com/dakanji/RefindPlusUDK" edk2
 		;;
 	*)
+	AcidantheraAUDK)
+		git clone "https://github.com/acidanthera/audk" edk2
+		;;
+	*)
 		git clone "https://github.com/tianocore/edk2" edk2
 		git -C edk2 checkout "$EDK2_VERSION"
 		;;
 esac
 git -C edk2 submodule update --init
-
