78500e9046a382a40f49719ad79cb2ce64fca065
startergo <7897244+startergo@users.noreply.github.com>
2021-12-02 22:40:47 -0500

    Added more examples

---
+++ examples/github.com-acidanthera-OpenCore/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+mkdir -m 1777 out
+
+git clone --recursive https://github.com/acidanthera/OpenCorePkg OpenCorePkg
+docker pull xaionaro2/edk2-builder:AcudantheraAUDK
+
+# building DEBUG
+exec docker run --rm \
+    -e CFLAGS=-Wno-error \
+    -e TOOLCHAIN=CLANG38 \
+    -e ADDPATH=/usr/lib/llvm-13/bin \
+    -e BUILD_TARGET=DEBUG \
+    -e DSC_PATH=OpenCorePkg/OpenCorePkg.dsc \
+    -v "$PWD/OpenCorePkg/:/home/edk2/edk2/OpenCorePkg/" \
+    -v "$PWD/out:/home/edk2/Build" \
+    xaionaro2/edk2-builder:RefindPlusUDK
+    
+# building RELEASE
+exec docker run --rm \
+    -e CFLAGS=-Wno-error \
+    -e TOOLCHAIN=CLANG38 \
+    -e ADDPATH=/usr/lib/llvm-13/bin \
+    -e BUILD_TARGET=RELEASE \
+    -e DSC_PATH=OpenCorePkg/OpenCorePkg.dsc \
+    -v "$PWD/OpenCorePkg/:/home/edk2/edk2/OpenCorePkg/" \
+    -v "$PWD/out:/home/edk2/Build" \
+    xaionaro2/edk2-builder:RefindPlusUDK
