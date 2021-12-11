0ef980762055f305a11e7dedd1a809f15352e9f7
Dmitrii Okunev <xaionaro@dx.center>
2021-12-11 16:20:57 +0000

    optimize(git): Use --depth=1 and --recommend-shallow

--- clone-edk2.sh
+++ clone-edk2.sh
@@ -3,17 +3,18 @@
 echo "EDK2_VERSION:<$EDK2_VERSION>"
 case "$EDK2_VERSION" in
 	latest)
-		git clone "https://github.com/tianocore/edk2" edk2
+		git clone --depth=1 "https://github.com/tianocore/edk2" edk2
 		;;
 	RefindPlusUDK)
-		git clone "https://github.com/dakanji/RefindPlusUDK" edk2
+		git clone --depth=1 "https://github.com/dakanji/RefindPlusUDK" edk2
 		;;	
 	AcidantheraAUDK)
-		git clone "https://github.com/acidanthera/audk" edk2
+		git clone --depth=1 "https://github.com/acidanthera/audk" edk2
 		;;
 	*)
-		git clone "https://github.com/tianocore/edk2" edk2
+		git clone --depth=1 -b "$EDK2_VERSION" "https://github.com/tianocore/edk2" edk2 || \
+			git clone "https://github.com/tianocore/edk2" edk2
 		git -C edk2 checkout "$EDK2_VERSION"
 		;;
 esac
-git -C edk2 submodule update --init
+git -C edk2 submodule update --init --recommend-shallow
