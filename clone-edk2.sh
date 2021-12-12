de6fae172a82f09b24eeaf9db65881ed05b56686
startergo <7897244+startergo@users.noreply.github.com>
2021-12-11 22:04:04 -0800

    Update clone-edk2.sh

--- clone-edk2.sh
+++ clone-edk2.sh
@@ -5,10 +5,7 @@ case "$EDK2_VERSION" in
 	latest)
 		git clone --depth=1 "https://github.com/tianocore/edk2" edk2
 		;;
-	vUDK2018)
-		git clone -b UDK2018 "https://github.com/tianocore/edk2" edk2
-		;;
-	RefindPlusUDK)
+		RefindPlusUDK)
 		git clone --depth=1 "https://github.com/dakanji/RefindPlusUDK" edk2
 		;;
 	RefindPlusUDKJVT)
@@ -18,8 +15,8 @@ case "$EDK2_VERSION" in
 		git clone --depth=1 "https://github.com/acidanthera/audk" edk2
 		;;
 	*)
-		git clone "https://github.com/tianocore/edk2" edk2
-		git -C edk2 checkout "$EDK2_VERSION"
+		git clone --depth=1 -b "$EDK2_VERSION" "https://github.com/tianocore/edk2" edk2 || \
+ 			git clone "https://github.com/tianocore/edk2" edk2
 		;;
 esac
-git -C edk2 submodule update --init 
+git -C edk2 submodule update --init --recommend-shallow
