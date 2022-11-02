f5248de3b73651e8c43a816816766e80156f4e1c
Dmitrii Okunev <xaionaro@dx.center>
2022-11-02 14:36:40 +0000

    bugfix(github.com-dakanji-RefindPlus): Update the script

--- examples/github.com-dakanji-RefindPlus/build.sh
+++ examples/github.com-dakanji-RefindPlus/build.sh
@@ -8,24 +8,23 @@ docker pull xaionaro2/edk2-builder:RefindPlusUDK
 # hacky fix for duplication error of lodepng_malloc and lodepng_free
 sed -e 's/void[*] lodepng_malloc/void* _dup_lodepng_malloc/' \
     -e 's/void lodepng_free/void _dup_lodepng_free/' \
+	-e 's/void[*] lodepng_refit_malloc/void* _dup_lodepng_refit_malloc/' \
+    -e 's/void lodepng_refit_free/void _dup_lodepng_refit_free/' \
     -i-orig RefindPlusPkg/libeg/lodepng_xtra.c
 
-# building DEBUG
-exec docker run --rm \
-    -e CFLAGS=-Wno-error \
-    -e TOOLCHAIN=CLANG38 \
-    -e BUILD_TARGET=DEBUG \
-    -e DSC_PATH=RefindPlusPkg/RefindPlusPkg-DBG.dsc \
-    -v "$PWD/RefindPlusPkg/:/home/edk2/edk2/RefindPlusPkg/" \
-    -v "$PWD/out:/home/edk2/Build" \
-    xaionaro2/edk2-builder:RefindPlusUDK
+# hacky fix to remove "STATIC_ASSERT undefined" error
+cat > RefindPlusPkg/build.sh <<EOF
+sed -e 's%STATIC_ASSERT%//STATIC_ASSERT%' -i edk2/OpenCorePkg/Library/OcUnicodeCollationEngLib/OcUnicodeCollationEngCommon.c
+sed -e 's%"UnicodeLanguages%//"UnicodeLanguages%' -i edk2/OpenCorePkg/Library/OcUnicodeCollationEngLib/OcUnicodeCollationEngCommon.c
+/home/edk2/entry.sh
+EOF
+chmod +x RefindPlusPkg/build.sh
 
-# building RELEASE
-exec docker run --rm \
+docker run --rm \
     -e CFLAGS=-Wno-error \
     -e TOOLCHAIN=CLANG38 \
     -e BUILD_TARGET=RELEASE \
-    -e DSC_PATH=RefindPlusPkg/RefindPlusPkg-REL.dsc \
+    -e DSC_PATH=RefindPlusPkg/RefindPlusPkg.dsc \
     -v "$PWD/RefindPlusPkg/:/home/edk2/edk2/RefindPlusPkg/" \
     -v "$PWD/out:/home/edk2/Build" \
-    xaionaro2/edk2-builder:RefindPlusUDK
+    xaionaro2/edk2-builder:RefindPlusUDK /bin/bash /home/edk2/edk2/RefindPlusPkg/build.sh
